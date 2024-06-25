1d68ed3db4189f65fc962a9faeee43cd684b01ae
Armando233-abc <armandovigliotti5@gmail.com>
2024-06-25 17:03:00 +0200

    Create method for connection and realese connection

--- src/main/java/model/DriverManagerConnectionPool.java
+++ src/main/java/model/DriverManagerConnectionPool.java
@@ -20,5 +20,41 @@ public class DriverManagerConnectionPool {
 		}
 	}
 	
+	private static Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+		String db = "GeekFactoryDB";
+		String username = "root";
+		String password = "root";
+
+		newConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+db, username, password);
+		
+		newConnection.setAutoCommit(false);
+		
+		return newConnection;
+	}
+	
+	public static synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+		
+		if(! freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			DriverManagerConnectionPool.freeDbConnections.remove(0);
+			try {
+				if (connection.isClosed()) {
+					connection = DriverManagerConnectionPool.getConnection();
+				}
+			} catch (SQLException e) {
+				connection = DriverManagerConnectionPool.getConnection();
+			}
+		}
+		else connection = DriverManagerConnectionPool.createDBConnection();
+		
+		return connection;
+	}
+	
+	public static synchronized void releaseConnection(Connection connection) {
+		DriverManagerConnectionPool.freeDbConnections.add(connection);
+	}
+	
 	
 }
