7248ed6156e4db92b019385bd87aacd369f960fa
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-06-25 23:19:39 +0200

    Create all bean

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -22,59 +22,60 @@ public class Utente {
 		dataDiNascita = null;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPsw(String psw) {
-		this.psw = psw;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
-
-	public void setIsAdmin(Boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
-
-	public void setDataDiNascita(Date dataDiNascita) {
-		this.dataDiNascita = dataDiNascita;
-	}
-
 	public String getEmail() {
 		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getPsw() {
 		return psw;
+	}
+
+	public void setPsw(String psw) {
+		this.psw = psw;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public String getCognome() {
 		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
 	}
 
 	public Boolean getIsAdmin() {
 		return isAdmin;
 	}
 
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
 	public Date getDataDiNascita() {
 		return dataDiNascita;
 	}
+
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
 }
