241de1010f4c4aac4b8352ec3960230a372b7a5f
Antonio-Caiazzo <antoniocaiazzo2003@gmail.com>
2024-07-01 14:51:49 +0200

    update DB

--- database/creazione_e_popolamento_database.sql
+++ database/creazione_e_popolamento_database.sql
@@ -51,8 +51,9 @@ CREATE TABLE prodotto (
     descrizione TEXT,
     costo DECIMAL(10, 2) NOT NULL,
     sesso ENUM('u', 'd') NOT NULL,
-    immagine VARCHAR(255) NOT NULL,
-    categoria VARCHAR(255) NOT NULL
+    immagine VARCHAR(512) NOT NULL,
+    categoria VARCHAR(255) NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE
 );
 
 DROP TABLE IF EXISTS carrello;
@@ -70,6 +71,7 @@ CREATE TABLE contiene (
     ordine_codice INT,
     prodotto_codice INT,
     quantita INT,
+    prezzo DECIMAL(10, 2),
     PRIMARY KEY (ordine_codice, prodotto_codice),
     FOREIGN KEY (ordine_codice) REFERENCES ordine(codice) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (prodotto_codice) REFERENCES prodotto(codice) ON DELETE CASCADE ON UPDATE CASCADE
@@ -98,13 +100,6 @@ INSERT INTO carta(numero_carta, scadenza_carta, nome_titolare, cognome_titolare,
     ('4444555566667777', '2023-11-30', 'Jeff', 'Bezos', 'jeffbezosg@gmail.com'),
     ('5555666677778888', '2025-07-31', 'Larry', 'Page', 'larrypage@gmail.com');
 
-INSERT INTO ordine(data, costo_totale, utente_email) VALUES
-    ('2024-06-01', 150.00, 'admin@admin.com'),
-    ('2024-06-02', 200.50, 'user@user.com'),
-    ('2024-06-03', 300.75, 'markzuckerberg@gmail.com'),
-    ('2024-06-04', 180.25, 'jeffbezosg@gmail.com'),
-    ('2024-06-05', 250.80, 'larrypage@gmail.com');
-
 INSERT INTO prodotto(nome, descrizione, costo, sesso, immagine, categoria) VALUES
     ('Maglia bianca', 'Maglia di cotone bianca', 29.99, 'u', 'maglia_bianca.jpg', 't-shirt'),
     ('Pantaloni neri', 'Pantaloni aderenti neri', 49.99, 'u', 'pantaloni_neri.jpg', 'pantaloni'),
@@ -132,31 +127,5 @@ INSERT INTO prodotto(nome, descrizione, costo, sesso, immagine, categoria) VALUE
     ('Orecchini perle', 'Orecchini eleganti perle', 29.99, 'd', 'orecchini_perle.jpg', 'gioielli'),
     ('Polo rosa', 'Polo casual rosa', 34.99, 'u', 'polo_rosa.jpg', 't-shirt'),
     ('Felpa con cappuccio', 'Felpa con cappuccio nera', 69.99, 'd', 'felpa_cappuccio.jpg', 'felpe');
-
-INSERT INTO carrello(utente_email, prodotto_codice, quantita) VALUES
-    ('user@user.com', 1, 1);  -- 1x Maglia bianca
-
-INSERT INTO contiene(ordine_codice, prodotto_codice, quantita) VALUES
-    (1, 1, 1),  -- 1x Maglia bianca
-    (1, 3, 2),  -- 2x Camicia a righe
-    (1, 5, 1);  -- 1x Jeans blu
-
-INSERT INTO contiene(ordine_codice, prodotto_codice, quantita) VALUES
-    (2, 2, 1),  -- 1x Pantaloni neri
-    (2, 4, 1),  -- 1x Abito rosso
-    (2, 7, 1);  -- 1x Sciarpa grigia
-
-INSERT INTO contiene(ordine_codice, prodotto_codice, quantita) VALUES
-    (3, 6, 1),  -- 1x Giacca invernale
-    (3, 8, 3);  -- 3x Calze sportive
-
-INSERT INTO contiene(ordine_codice, prodotto_codice, quantita) VALUES
-    (4, 9, 1),  -- 1x Cappello di lana
-    (4, 10, 2); -- 2x Gonna nera
-
-INSERT INTO contiene(ordine_codice, prodotto_codice, quantita) VALUES
-    (5, 11, 1),  -- 1x Polo blu
-    (5, 12, 1),  -- 1x Stivaletti marroni
-    (5, 13, 1);  -- 1x Occhiali da sole
 
 UNLOCK TABLES;
